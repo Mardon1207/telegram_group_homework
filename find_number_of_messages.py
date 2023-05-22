217be9184a563659dfd823bb0d0671739bce270a
Mardon1207 <mardonsultonov8@gmail.com>
2023-05-22 17:32:02 +0500

    number

--- find_number_of_messages.py
+++ find_number_of_messages.py
@@ -8,6 +8,13 @@ def find_number_of_messages(data: dict)->int:
         data (dict): Dictionary containing the data of the json file.
     Returns:
         int: Total number of messages.
-    
+
     """
-    return
+    l=0
+    for i in data['messages']:
+        if i["type"]=="message":
+            l+=1
+    return l
+file_path='data/result.json'
+data=read_data(file_path)
+print(find_number_of_messages(data))
